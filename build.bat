68b0fbd6e28b96a95715cc96be8735e8130bd7c4
Scott Loyd <loyd.scott@gmail.com>
2018-03-06 20:00:28 -0500

    renamed files

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cl /Zi toy.cpp /link /out:kal.exe kernel32.lib user32.lib dwrite.lib d2d1.lib gdi32.lib
+cl /Zi editor.cpp /link /out:editor.exe kernel32.lib user32.lib dwrite.lib d2d1.lib gdi32.lib
 
 @echo on
